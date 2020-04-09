77f67c18f9c99870840a10119a5d0f53d56c2254
Chris Moses <chris_moses@yahoo.com>
2020-04-09 14:49:56 -0400

    Add files
    Added the code for the project

---
+++ BmpToCsv/BmpToByte1/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace BmpToByte1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.txtOutputFilePath = new System.Windows.Forms.TextBox();
+            this.txtImageFilePath = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(270, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create CSV";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lblWidth);
+            this.splitContainer1.Panel1.Controls.Add(this.lblHeight);
+            this.splitContainer1.Panel1.Controls.Add(this.txtOutputFilePath);
+            this.splitContainer1.Panel1.Controls.Add(this.txtImageFilePath);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1634, 831);
+            this.splitContainer1.SplitterDistance = 665;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(302, 432);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(151, 37);
+            this.lblWidth.TabIndex = 4;
+            this.lblWidth.Text = "Width:XX";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(70, 432);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(160, 37);
+            this.lblHeight.TabIndex = 3;
+            this.lblHeight.Text = "Height:XX";
+            // 
+            // txtOutputFilePath
+            // 
+            this.txtOutputFilePath.Location = new System.Drawing.Point(59, 171);
+            this.txtOutputFilePath.Name = "txtOutputFilePath";
+            this.txtOutputFilePath.Size = new System.Drawing.Size(557, 44);
+            this.txtOutputFilePath.TabIndex = 2;
+            this.txtOutputFilePath.Text = "c:\\Images\\output1.csv";
+            // 
+            // txtImageFilePath
+            // 
+            this.txtImageFilePath.Location = new System.Drawing.Point(59, 56);
+            this.txtImageFilePath.Name = "txtImageFilePath";
+            this.txtImageFilePath.Size = new System.Drawing.Size(557, 44);
+            this.txtImageFilePath.TabIndex = 1;
+            this.txtImageFilePath.Text = "c:\\Images\\20x20.bmp";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(965, 831);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1634, 831);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox txtOutputFilePath;
+        private System.Windows.Forms.TextBox txtImageFilePath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Label lblWidth;
+    }
+}
+
---
+++ BmpToCsv/BmpToByte1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace BmpToByte1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //image to byteArray
+            string source = txtImageFilePath.Text;
+            //Image img = Image.FromFile(source);
+            Bitmap img = new Bitmap(source);
+            int h = img.Height;
+            int w = img.Width;
+            lblHeight.Text = "Height: " + h.ToString();
+            lblWidth.Text = "Width: " + w.ToString();
+            string output = txtOutputFilePath.Text;
+
+
+            int count = 0;
+            Color pixelColor;
+            int red = 0;
+            int blue = 0;
+            int green = 0;
+            int colorSum = 0;
+            using (StreamWriter writer = new StreamWriter(output))
+            {
+                for (int width = 0; width < w; width++)
+                {
+                    for (int height = 0; height < h; height++)
+                    {
+                        pixelColor = img.GetPixel(width, height);
+                        red = pixelColor.R;
+                        green = pixelColor.G;
+                        blue = pixelColor.B;
+                        if (red == 0 && green == 0 && blue == 0)
+                        {
+                            colorSum = 0;
+                        }
+                        else
+                        {
+                            colorSum = (red + blue + green) / 3;
+                        }
+                        Console.WriteLine(count + " Height= " + height + " width = " + width + " Color = " + pixelColor + " Intensity = " + colorSum.ToString());
+                        writer.Write(colorSum.ToString() + ",");
+                        count++;
+                    }
+                    writer.Write(Environment.NewLine);
+                }
+                writer.Flush();
+            }
+
+        }
+    }
+}
